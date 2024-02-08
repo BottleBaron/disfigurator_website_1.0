1cb2ceb1cc032d39132cd029eddab0a04cc189c5
Bottle_Baron <petrus.h.johansson@gmail.com>
2024-02-08 15:55:58 +0100

    Added gitignore

--- Back/Program.cs
+++ Back/Program.cs
@@ -1,9 +1,27 @@
+using Back.Model;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 // Sida för tutorial: https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-8.0&tabs=visual-studio
+
+
+string? connectionString = Environment.GetEnvironmentVariable("PLACEHOLDER");
+
+
+if (connectionString == null)
+{
+    Console.WriteLine("You must set your 'MONGODB_URI' environment variable. To learn how to set it, see https://www.mongodb.com/docs/drivers/csharp/current/quick-start/#set-your-connection-string");
+    Environment.Exit(0);
+}
+var client = new MongoClient(connectionString);
+var db = PostContext.Create(client.GetDatabase("seeded_db"));
+
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
--- Back/obj/Debug/net8.0/Back.AssemblyInfo.cs
+++ Back/obj/Debug/net8.0/Back.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be83fc76a57671cc5bde2a784676e6fb3ff226a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
